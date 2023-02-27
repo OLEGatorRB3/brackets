4d67ebe2d918a0513ff5b10836e3b56df9a9084a
OLEGatorRB3 <olegatorrb3@gmail.com>
2023-02-27 17:04:02 +0300

    docs: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+    // ['(', '(', '(', ')', ')', ')', '(', ')']
+
+    let obj = bracketsConfig.reduce((acc, cur) => {
+        acc[cur[1]] = cur[0]
+        return acc
+    }, {})
+    console.log(obj)
+    // {
+        // ): '(',
+        // ]: '[', 
+        // }: '{', 
+        // |: '|'
+    // }
+    let brackets = Object.values(obj)
+    console.log(brackets)
+
+    let stek = [];
+    for(let i = 0; i < str.length; i++){
+        let cur = str[i];
+        // console.log(cur)
+
+        if(brackets.includes(cur)){
+            stek.push(cur)
+        }         
+        let lastElement = stek[stek.length - 1];
+        console.log(lastElement)
+
+        if(obj[cur] == lastElement) {
+            stek.pop()
+        } 
+        
+        
+    }
+    console.log(stek)
+    return (stek.length == 0)
 }
